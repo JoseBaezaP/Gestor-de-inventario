cd74376fb10e8633ee749b8fa2a34cf546f3aa81
jose de la rosa <josedelarosabass@gmail.com>
2021-11-29 00:19:18 -0600

    test

--- resources/views/Components/allstores/modal.blade.php
+++ resources/views/Components/allstores/modal.blade.php
@@ -4,17 +4,13 @@
   </x-slot>
   <x-slot name="content">
     <x-jet-label class="text-base">Nombre</x-jet-label>
-    <x-jet-input name="name" type="text" class="w-full my-2" value="{{$store?->name}}" placeholder="Nombre de la tienda"
-      disabled />
+    <x-jet-input name="name" type="text" class="w-full my-2" placeholder="Nombre de la tienda" disabled />
     <x-jet-label class="text-base">Dirección</x-jet-label>
-    <x-jet-input name="address" type="text" class="w-full my-2" value="{{$store?->address}}"
-      placeholder="Direccion de la tienda" disabled />
+    <x-jet-input name="address" type="text" class="w-full my-2" placeholder="Direccion de la tienda" disabled />
     <x-jet-label class="text-base">Teléfono</x-jet-label>
-    <x-jet-input name="phone" type="number" class="w-full my-2" value="{{$store?->phone}}"
-      placeholder="Teléfono de la tienda" disabled />
+    <x-jet-input name="phone" type="number" class="w-full my-2" placeholder="Teléfono de la tienda" disabled />
     <x-jet-label class="text-base">Encargado encargado</x-jet-label>
-    <x-jet-input name="phone" type="text" class="w-full my-2" value="{{$store?->user->name}}"
-      placeholder="Teléfono de la tienda" disabled />
+    <x-jet-input name="phone" type="text" class="w-full my-2" placeholder="Teléfono de la tienda" disabled />
   </x-slot>
   <x-slot name="footer">
     <x-jet-danger-button wire:click="$set('visible', false)">Cancelar</x-jet-danger-button>

d9884959b110c7bea08bf2008cfd996cbdd23d7a
jose de la rosa <josedelarosabass@gmail.com>
2021-11-29 01:11:03 -0600

    test4

--- resources/views/livewire/employee/allstores.blade.php
+++ resources/views/livewire/employee/allstores.blade.php
@@ -4,22 +4,7 @@
       <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-10 grid grid-row-3">
         <x-jet-input type="text" wire:model="search" class="mb-5 w-full sm:w-1/2"
           placeholder="Escriba el nombre del producto" />
-        <x-stores.table>
-          @foreach ($products as $product)
-          @if($product->quantity)
-          <tr>
-            <td class="border p-5">{{$product->name}}</td>
-            <td class="border p-5 text-center">{{$product->quantity}}</td>
-            <td class="border p-5 text-center">{{number_format($product->price,2)}}</td>
-            <td class="border p-5 relative text-center">
-              <span>{{$product->store->name}}</span>
-              <button wire:click="show({{$product->store->id}})"
-                class="absolute bg-gray-300 w-4 rounded-full text-xs ml-2">i</button>
-            </td>
-          </tr>
-          @endif
-          @endforeach
-        </x-stores.table>
+
         <div class="my-5">
           {{ $products->links() }}
         </div>
